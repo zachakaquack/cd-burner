b417669ca7cec32a0e9c91c40563205ddac94d50
Zach Allen <zach12399@gmail.com>
2026-08-20 19:16:00 -0500

    insert tags from filename

--- src/youtube_downloader.py
+++ src/youtube_downloader.py
@@ -18,6 +18,10 @@ def start_download(url: str) -> list[Song]:
 
     download_video(url)
     video_title = get_the_metadata_just_for_the_title_of_the_playlist_lol(url)
+
+    # since these are from youtube, we have to use the chapter name to construct
+    # the tags BEFORE creating the song objects (which use the metadata tags).
+    insert_tags(songs, video_title)
     songs = parse_songs()
 
     # each Song has a `.path` member var, which gets updated after this function
@@ -40,6 +44,48 @@ def get_the_metadata_just_for_the_title_of_the_playlist_lol(url: str) -> str:
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:  # pyright: ignore[reportArgumentType]
         info = ydl.extract_info(url)
         return info.get("title", "Unknown")  # pyright: ignore[reportReturnType]
+
+
+def insert_tags(songs: list[Song], video_title: str) -> None:
+    def find_info(filename: str) -> tuple[int, str, str]:
+        """
+        gets the information from the filename.
+        return a tuple of: (
+            track_num: int,
+            title: str,
+            artist: str,
+        )
+        inputted filename should look like: "%(section_number)02d. %(section_title)s.%(ext)s"
+        e.g. "05. My Super Awesome Song Name - Your Epic Artist Name.extension"
+        (the title & artist are both in the section title)
+        """
+        track_num: int = 0
+        artist: str = ""
+        title: str = ""
+
+        track_num = int(
+            filename[:2]
+        )  # 2 because track_num is formatted as DD. where D is a digit
+        filename = filename[4:]  # 4 because the first 4 chars are always "DD. "
+
+        # artist is first; and in the weird case of Midsummer - Flamingosis (Ehted kadunud - Collage),
+        # title is Midsummer and artist is Flamingosis... so only break once
+        title, artist = filename.split(" - ", maxsplit=1)
+
+        # artist is now "{ARTIST}.extension" :(
+        artist = artist[: artist.rfind(".")]
+
+        return (track_num, artist, title)
+
+    for song in songs:
+        track_num, title, artist = find_info(song.path.stem)
+
+        flac = FLAC(f"{song.path}")
+        flac["ALBUM"] = video_title
+        flac["TITLE"] = title
+        flac["ARTIST"] = artist
+        flac["track"] = flac["track_num"] = flac["trkn"] = str(track_num)
+        flac.save()  # pyright: ignore[reportUnknownMemberType]
 
 
 def encode_songs(songs: list[Song]) -> None:
